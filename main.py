373343572d8a7613c5d2b2130d1f2baf4644bce6
jdvfx <ju.dep@zoho.com>
2024-09-25 23:33:29 -0700

    added basic pie chart

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+import matplotlib.pyplot as plt
 import json
 import glob
 import re
@@ -7,11 +9,11 @@ from operator import itemgetter
 # load categories filters
 categories = "categories.json"
 with open(categories,'r') as file:
-    d = json.load(file)
+    cat_dict = json.load(file)
 
 
 k ={}
-for key,value in d.items():
+for key,value in cat_dict.items():
 
     escaped_pattern = re.escape(key).replace(r'\*', '.*')
     escaped_pattern = escaped_pattern.upper()
@@ -95,11 +97,9 @@ for month in months:
         cat_val[val]=0.0
     cat_val["other"]=0.0
 
-
     for transaction in month_list:
         cat = transaction["cat"]
         debit = transaction["debit"]
-        
         cat_val[cat]=cat_val[cat] + float(debit)
 
 
@@ -126,12 +126,92 @@ for month in months:
             l=""
             log.append(l)
 
-for line in log:
-    print(line)
+# for line in log:
+#     print(line)
 
-# with open("log.txt","w") as file:    
-#     for line in log:
-#         file.write(line+"\n")
-#         print(line)
+with open("log.txt","w") as file:    
+    for line in log:
+        file.write(line+"\n")
+        print(line)
+
+# ////////////////////////////////////////
+
+month = months[-2]
+
+# create a list for the items of this month
+month_list = []
+pie={}
+for match in matches:
+    m = match.get("month")
+    if m==month:
+        month_list.append(match)
+
+cat_val={}
+for key,val in k.items():
+    cat_val[val]=0.0
+cat_val["other"]=0.0
+
+for transaction in month_list:
+    cat = transaction["cat"]
+    debit = transaction["debit"]
+    cat_val[cat]=cat_val[cat] + float(debit)
+
+# -- display percentages
+total=0
+for key,val in cat_val.items():
+    total+=val
+
+for key,val in cat_val.items():
+    percentage = (val*100.0)/total
+    if percentage>0:
+        pie[key] = percentage
+#
+
+pie = dict(sorted(pie.items(),key=itemgetter(1)))
+
+labels = list(pie.keys())
+sizes = list(pie.values())
+
+colors_ = [\
+ "#ffaa00",\
+ "#2279ff",\
+ "#2bc395",\
+ "#2c9440",\
+ "#c866ff",\
+ "#d3302e",\
+ "#e6ecec",\
+ "#fc552e",\
+ "#ff77bb",\
+ "#212c3e",\
+ "#4088cc",\
+ "#2bc395",\
+ "#32a448",\
+ "#c866ff",\
+ "#aa2255",\
+ "#afb6b9",\
+ "#bb33bb"\
+]
+
+
+dict_colors={}
+d = list(set(cat_dict.values()))
+d.sort()
+for idx,i in enumerate(d):
+    dict_colors[i] = colors_[idx]
+
+dict_colors["other"] = "#888888"
+
+colors = []
+for idx,i in enumerate(labels):
+    col = dict_colors.get(i)
+    colors.append(col)
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%',
+        shadow=False, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.show()
+                    
 
 
